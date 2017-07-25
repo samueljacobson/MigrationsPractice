dafa543b9da9ff9056539240a37a4f47ebcff874
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-25 11:50:59 -0400

    completed migration

---
+++ MigrationsPractice/Models/MigrationsPracticeContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MigrationsPractice.Models
+{
+    public class MigrationsPracticeContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MigrationsPracticeContext() : base("name=MigrationsPracticeContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MigrationsPractice.Models.Score> Scores { get; set; }
+    }
+}
